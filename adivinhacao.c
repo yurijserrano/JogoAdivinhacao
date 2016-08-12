4102c30cf345e79514ca96d5e91c980c14b2764f
Yuri Jose Dos Santos Serrano <yurijserrano@gmail.com>
2016-08-12 14:38:07 -0300

    Inclusão da inserção do valor do chute

--- adivinhacao.c
+++ adivinhacao.c
@@ -3,11 +3,22 @@
 
 int main(){
 
+
 	/*
 		Imprime o cabeçalho do jogo
 	*/
 	printf("******************************************\n");
 	printf("* Bem Vindo ao nosso jogo de adivinhação *\n");
 	printf("******************************************\n");
+
+	int numeroSecreto = 42;
+	int chute =0;
+
+	//printf("O número %d é o secreto. Não Conta pra ninguém\n",numeroSecreto);
+
+	printf("Qual é o sue chute?");
+	scanf("%d",&chute);
+	printf("Seu chute foi %d",chute);
+
 	return 0;
 }
