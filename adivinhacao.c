10da01d41a65349c2e01cd749a98f2bd62306d29
Yuri Jose Dos Santos Serrano <yurijserrano@gmail.com>
2016-08-14 03:23:05 -0300

    Inclusao de uma constante que simboliza o número de tentativas

--- adivinhacao.c
+++ adivinhacao.c
@@ -2,12 +2,10 @@
 #include <stdlib.h>
 #include <locale.h>
 
+#define NUMERO_DE_TENTATIVAS 5
+
 
 int main(){
-
-
-
-
 
 	/*
 		Corrige a acentuação no terminal
@@ -30,19 +28,20 @@ int main(){
 	//printf("O número %d é o secreto. Não Conta pra ninguém\n",numeroSecreto);
 
 
-	for(i = 1; i<=3 ;i++)
+	for(i = 1; i<=NUMERO_DE_TENTATIVAS ;i++)
 	{
 
-		printf("Tentativa %d de 3\n",i);
+		printf("Tentativa %d de %d\n",i,NUMERO_DE_TENTATIVAS);
 		printf("Qual é o seu chute?");
 		scanf("%d",&chute);
 		printf("Seu chute foi %d",chute);
 		int acertou = (chute == numeroSecreto);
-		
+
 		if(acertou)
 		{
 			printf("Parabéns Você Acertou !!!!!!!\n");
 			printf("Jogue Novamente, você é um bom jogador !!!!\n");
+			break;
 		}
 		else
 		{
@@ -60,6 +59,6 @@ int main(){
 		
 		}
 	}
-
+	printf("Fim de Jogo!!!!\n");
 	return 0;
 }
